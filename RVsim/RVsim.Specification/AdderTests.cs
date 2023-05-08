a8e3e885919174cf930786a7d34a792a6b41dfa7
Bunch of Bits <aloy@worldonline.nl>
2023-05-08 23:45:44 +0200

    Added carry in/out to the adder

--- RVsim/RVsim.Specification/AdderTests.cs
+++ RVsim/RVsim.Specification/AdderTests.cs
@@ -8,18 +8,22 @@ namespace RVsim.Specification
   {
     private Source<uint> _a;
     private Source<uint> _b;
+    private Source<bool> _ci;
     private Adder _dut;
-    private Sink<uint> _sink;
+    private Sink<uint> _sum;
+    private Sink<bool> _co;
 
     [SetUp]
     public void Setup()
     {
       _a = new Source<uint>("A");
       _b = new Source<uint>("B");
+      _ci = new Source<bool>("Ci");
 
-      _dut = new Adder("A1", _a.Port, _b.Port);
+      _dut = new Adder("A1", _a.Port, _b.Port, _ci.Port);
 
-      _sink = new Sink<uint>(_dut.Sum);
+      _sum = new Sink<uint>(_dut.Sum);
+      _co = new Sink<bool>(_dut.Co);
     }
 
     [Test]
@@ -27,8 +31,32 @@ namespace RVsim.Specification
     {
       _a.Set(1u);
       _b.Set(4u);
+      _ci.Set(false);
 
-      Assert.That(_sink.Value, Is.EqualTo(5));
+      Assert.That(_sum.Value, Is.EqualTo(5u));
+      Assert.That(_co.Value, Is.False);
+    }
+
+    [Test]
+    public void CheckAdditionWithOverflow()
+    {
+      _a.Set(2_000_000_000u);
+      _b.Set(4_000_000_000u);
+      _ci.Set(false);
+
+      Assert.That(_sum.Value, Is.EqualTo(1_705_032_704u));  // (2E9 + 4E9) mod 2^32
+      Assert.That(_co.Value, Is.True);
+    }
+
+    [Test]
+    public void CheckAdditionWithCarryIn()
+    {
+      _a.Set(1u);
+      _b.Set(4u);
+      _ci.Set(true);
+
+      Assert.That(_sum.Value, Is.EqualTo(6u));
+      Assert.That(_co.Value, Is.False);
     }
   }
 }
--- RVsim/RVsim/Adder.cs
+++ RVsim/RVsim/Adder.cs
@@ -8,23 +8,34 @@ namespace RVsim
   {
     private readonly Port<uint> _a;
     private readonly Port<uint> _b;
+    private readonly Port<bool> _ci;
 
-    public Adder(string name, Port<uint> a, Port<uint> b)
+    public Adder(string name, Port<uint> a, Port<uint> b, Port<bool> ci)
     {
       _a = a;
       _b = b;
+      _ci = ci;
 
       Sum = new Port<uint>(name, nameof(Sum));
+      Co = new Port<bool>(name, nameof(Co));
 
       _a.PortChanged += Update;
       _b.PortChanged += Update;
+      _ci.PortChanged += Update;
     }
 
     public Port<uint> Sum { get; }
 
+    public Port<bool> Co { get; }
+
     private void Update(object sender, EventArgs args)
     {
-      Sum.Value = _a.Value + _b.Value;
+      var a = _a.Value;
+      var b = _b.Value;
+      var sum = a + b + (_ci.Value ? 1u : 0u);
+
+      Sum.Value = sum;
+      Co.Value = sum < a; // or sum < b, either works
     }
   }
 }
