f9175f9341e7d740537a099aba3c5de94a8a1717
Bunch of Bits <vanallesenmeer@kpnmail.nl>
2024-10-28 10:01:05 +0100

    Added a comment to explain why the Schedule() method should not be "optimized"

--- RVsim/PicoSim/Scheduler.cs
+++ RVsim/PicoSim/Scheduler.cs
@@ -25,6 +25,30 @@ internal class Scheduler
   internal void Schedule<T>(Port<T> port, T value)
     where T : unmanaged, IComparable<T>, IConvertible
   {
+    // Do NOT check old/new value equality here and skip adding in case of
+    // equality in a vain attempt to speed up simulation !!!
+    //
+    // "Now" may still contain assignments for "port" further down the line,
+    // which may need to be overriden again with the "new" value
+    //
+    // Consider for example a 2-bit ripple carry adder, with two inputs, A and B,
+    // sum output S and carry output C. The result should be 00 with a carry.
+    // At T = 0, assign 01 to A, and when all consequences have been processed,
+    // assign 11 to B at T = 2. Assume al ports are 0 at startup. Index 0 is
+    // the least significant bit.
+    //
+    //     T = 0        T = 1        T = 2        T = 3        T = 4
+    // A[0] := 1 => S[0] := 1
+    // A[1] := 0 => -
+    //                           B[0] := 1 => S[0] := 0
+    //                                        C[0] := 1 => S[1] := 0
+    //                                                     C[1] := 1
+    //                           B[1] := 1 => S[1] := 1
+    //
+    // If, at T = 3, you would decide not to schedule S[1] := 0 for T = 4 as a
+    // consequence of C[0] := 1 because S[1] is still 0 at that moment, you
+    // would ignore S[1] := 1 at T = 3, and would end up with 10 with a carry
+    // as a result, instead of 00 with a carry
     _future[port] = value;
   }
 
